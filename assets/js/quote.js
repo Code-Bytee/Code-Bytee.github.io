116b9ac97e412ca1cd68cd2742c2cdf698915091
Code-Bytee <codery114187@gmail.com>
2021-09-26 17:50:48 +0530

    Added Quote Generator

---
+++ assets/js/quote.js
@@ -0,0 +1,46 @@
+
+var i = 0;
+var speed = 25;
+var quote;
+var HttpClient = function() {
+  this.get = function(url, aCallback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200)
+        aCallback(request.responseText);
+
+    }
+    request.open("GET", url, true);
+    request.send(null);
+  }
+}
+function typeWriter() {
+  if (i < quote.content.length) {
+    document.getElementById("msg").innerHTML += quote.content.charAt(i);
+    i++;
+
+    setTimeout(typeWriter, speed);
+  }
+  document.getElementById("author").innerHTML =
+    quote.author;
+}
+
+function getQuote() {
+
+  if (i > 0) {
+
+    document.getElementById("msg").innerHTML = "";
+    i = 0;
+  }
+  client = new HttpClient();
+  client.get("https://api.quotable.io/random", function(res) {
+    quote = JSON.parse(res);
+    //  alert(quote.content);
+    typeWriter();
+
+  });
+
+
+
+}
+getQuote();
