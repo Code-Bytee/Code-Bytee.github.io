3cdeb29d8cc4092df74c06db7202f62b6568508b
Code-Bytee <codery114187@gmail.com>
2021-10-03 09:06:16 +0530

    updated

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,4 +1,3 @@
-
 var quote;
 var HttpClient = function() {
   this.get = function(url, aCallback) {
@@ -6,51 +5,42 @@ var HttpClient = function() {
     request.onreadystatechange = function() {
       if (request.readyState == 4 && request.status == 200)
         aCallback(request.responseText);
-
     }
     request.open("GET", url, true);
     request.send(null);
   }
 }
-  
+ran = Math.floor(Math.random() * 3 + 1)
+themes = ["Megatron", 'pinkFish', 'OrangeCoral', 'cherryBlossom'];
+var container = document.getElementsByClassName("container")[0];
+container.classList.add(themes[ran]);
+container.style.display = "flex";
+console.log('Theme : ' + themes[ran]);
 
 function getQuote() {
-if(quote.content.length<200){
-console.log(quote.content.length)
-  document.getElementById("msg").innerHTML = quote.content;
-  document.getElementById('author').innerHTML = quote.author;
-}
-else{
-
-fetchQuote();
-
-}
-
+  if (quote.content.length < 200) {
+    console.log(quote.content.length)
+    document.getElementById("msg").innerHTML = quote.content;
+    document.getElementById('author').innerHTML = quote.author;
+  }
   
+  else {
+    fetchQuote();
+  }
 }
-function next(){
-  document.getElementById("msg").innerHTML = "";
-  document.getElementById("author").innerHTML = "";
+
+  function next() {
+    document.getElementById("msg").innerHTML = "";
+    document.getElementById("author").innerHTML = "";
+    fetchQuote();
+  }
+
+  function fetchQuote() {
+    client = new HttpClient();
+    client.get("https://api.quotable.io/random", function(res) {
+      quote = JSON.parse(res);
+      getQuote();
+    });
+  }
   fetchQuote();
-}
-function fetchQuote(){
-				
-
-client = new HttpClient();
-  client.get("https://api.quotable.io/random", function(res) {
-    quote = JSON.parse(res);
-    getQuote();
-   });   
-   }
-fetchQuote();
-//getQuote();
-
- 
-
-
-  
-
-
-
-
-				
+  console.log(quote.content);
